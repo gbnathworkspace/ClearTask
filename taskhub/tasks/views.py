b7e4c22418eba3486c342cd9d279380509655b96
hemanathr <hemanathjack@gmail.com>
2024-09-21 20:39:52 +0530

    remove test merge

--- taskhub/tasks/views.py
+++ taskhub/tasks/views.py
@@ -3,6 +3,3 @@ from django.http import JsonResponse
 
 def hello_world(request):
     return JsonResponse({'message': 'Hello, World!'})
-
-def hello_name(request, name):
-    return JsonResponse({'message': f'Hello, {name}!'})
